84d19eeeb378d14d0f09abdfef29118cdf86d65b
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-05-11 14:40:23 +0000

    weather app complete

--- script.js
+++ script.js
@@ -53,6 +53,28 @@ function init(resultFromServer) {
     let windSpeedElement = document.getElementById("windSpeed");
     let cityHeader = document.getElementById("cityHeader");
     let weatherIcon = document.getElementById("documentIconImg");
+
+    weatherIcon.src = "http://openweathermap.org/img/w/" + resultFromServer.weather[0].icon + ".png";
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + "&#176";
+    windSpeedElement.innerHTML = "Winds at " + Math.floor(resultFromServer.wind.speed) + " m/s";
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = "Humidity levels at " + resultFromServer.main.humidity + "%";
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById("weatherContainer");
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/2}px)`;
+    weatherContainer.style.visibility = "visible";
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
